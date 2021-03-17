56b0320194694aa57fc5c2ce1e87776a7451a54a
ankien <52425502+ankien@users.noreply.github.com>
2021-03-17 01:10:54 -0400

    some ppu changes

--- Desu/hardware/LCD.cpp
+++ Desu/hardware/LCD.cpp
@@ -28,34 +28,69 @@ LCD::LCD() {
     compileShaders();
 }
 
+#define BITMAP_MODE_SETUP uint16_t lineStart = VCOUNT * 240; \
+                          uint16_t* scanLine = pixelBuffer + lineStart;
 void LCD::fetchScanline() {
 
-    uint16_t lineStart = VCOUNT * 240;
+    /*
+        - Quick 101 on the PPU -
+        PRAM contains color data for BGs and Sprites/OBJs
+        Tiled BGs can range from, 128x128px to 1024x1024px, sprites up to 64x64px, each tiled BG + sprite is comprised of 8x8px bitmaps (tiles)
+        Palettes and tiles can be 4bpp(16colors/16banked palettes) or 8bpp(256colors/1palette), all affine BGs are 8bpp
+        Sprites have one tile-map entries/indexes (multi-tile), but BGs comprised of many (single-tile)
+        VRAM is basically a big bitmap 8 pixels wide, and store the tile data OR maps for tile BG modes (data for sprites in OAM as well)
+        Charblocks - 6 16kb chunks in VRAM, 4 for BGs and 2 for sprites, tile-indexing is different for sprites and BGs
+        
+        Idea behind fetch order of a scanline:
+        ?
+    */
+
+    // todo: implement rotation + scaling (affine), and objs for bitmap modes, check DISPCNT, no BGCNT in mode 3-5
     if(VCOUNT < 160) { // if VCOUNT < 160, load update a single scanline, 160-227 is non-visible scanline range
+
         switch(DISPCNT_MODE) {
-            case 0: // tile/map + text mode
+            case 0: // BG[0-3] text/tile BG mode, no affine
                 
                 break;
-            case 1: // mode 0/2 mixed
+            case 1: // BG[0-2] text/tile BGs like mode 0, but only BG2 has affine
                 break;
-            case 2: // tile/map + scale/rotation mode
+            case 2: // BG[2-3] tiled BGs w/ affine
                 break;
-            case 3: // bitmap mode for still images
-                for(uint8_t i = 0; i < 240; i++) {
-                    uint32_t vramIndex = (lineStart + i) * 2;
-                    pixelBuffer[lineStart + i] = *reinterpret_cast<uint16_t*>(&systemMemory->vram[vramIndex]);
-                }
-                break;
-            case 4: // bitmap mode
+            case 3: // BG[2] bitmap BG mode w/o page flipping, affine
             {
-                uint16_t frameBufferStart = DISPCNT_DISPLAY_FRAME_SELECT ? 0xA000 : 0;
-                for(uint8_t i = 0; i < 240; i++) {
-                    uint8_t paletteEntry = systemMemory->vram[frameBufferStart + lineStart + i] * 2;
-                    pixelBuffer[lineStart + i] = *reinterpret_cast<uint16_t*>(&systemMemory->pram[paletteEntry]);
-                }
+                BITMAP_MODE_SETUP
+                uint16_t* halfwordChunkVram = reinterpret_cast<uint16_t*>(&systemMemory->vram);
+                for(uint8_t i = 0; i < 240; i++)
+                    scanLine[i] = halfwordChunkVram[lineStart + i];
                 break;
             }
-            case 5: // bitmap mode
+            case 4: // BG[2] paletted bitmap BG mode, affine
+            {
+                BITMAP_MODE_SETUP
+                uint16_t frameBufferStart = DISPCNT_DISPLAY_FRAME_SELECT ? 0xA000 : 0;
+                uint16_t* halfwordChunkPram = reinterpret_cast<uint16_t*>(&systemMemory->pram);
+                for(uint8_t i = 0; i < 240; i++)
+                    // in paletted modes, pixels in an image are represented as 8-bit or 4-bit indexes into pram -
+                    // In the case of the bitmapped backgrounds in modes 3 and 4, pixels are represented as the 16-bit color values themselves.
+                    scanLine[i] = halfwordChunkPram[systemMemory->vram[frameBufferStart + lineStart + i]];
+                break;
+            }
+            case 5: // BG[2] bitmap BG mode with page flipping, affine
+            {
+                BITMAP_MODE_SETUP
+                uint8_t row = VCOUNT;
+                uint16_t frameBufferStart = DISPCNT_DISPLAY_FRAME_SELECT ? 0xA000 : 0;
+                uint16_t backgroundColor = *reinterpret_cast<uint16_t*>(&systemMemory->pram[0]);// background color is the first index in pram
+                if(row < 128) {
+                    uint16_t* halfwordChunkVram = reinterpret_cast<uint16_t*>(&systemMemory->vram);
+                    for(uint8_t i = 0; i < 160; i++)
+                        scanLine[i] = halfwordChunkVram[(row * 160 + i) + frameBufferStart];
+                    for(uint8_t i = 160; i < 240; i++)
+                        scanLine[i] = backgroundColor;
+                } else
+                    for(uint8_t i = 0; i < 240; i++)
+                        scanLine[i] = backgroundColor;
+            }
                 break;
         }
     }
